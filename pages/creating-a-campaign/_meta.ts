8f7be3c6813b40e8cfa3afc5506ae5458ed9c6f9
afk1997 <kaivandoshi1997@gmail.com>
2025-02-03 22:57:39 +0530

    range based incentives

--- pages/creating-a-campaign/_meta.ts
+++ pages/creating-a-campaign/_meta.ts
@@ -1,5 +1,6 @@
 const meta = {
     "kpi-campaign": "Add a KPI",
+    "rbi-campaign": "Add Range Based Incentive",
     "address-restrictions": "Add address restrictions",
     "liquidity-providers": "For liquidity providers",
 };

7d91e4ea68dc4b71d84892267c279f180d1c1324
luzzif <federico.luzzi@proton.me>
2025-02-26 17:26:15 +0000

    chore: update readme url

--- app/developers/react-library/page.tsx
+++ app/developers/react-library/page.tsx
@@ -10,7 +10,7 @@ const { wrapper: Wrapper, ...components } = getMDXComponents({
 
 export default async function Page() {
     const response = await fetch(
-        "https://raw.githubusercontent.com/metrom-xyz/monorepo/blob/b26c3fb56e1465787596c7743362b5c866349f19/packages/react/README.md",
+        `https://raw.githubusercontent.com/metrom-xyz/monorepo/b26c3fb56e1465787596c7743362b5c866349f19/packages/react/README.md`,
     );
     const data = await response.text();
     const rawJs = await compileMdx(data);

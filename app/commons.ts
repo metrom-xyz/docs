dc77c0b344a7697b6fa3414464b50b4ccf59c637
luzzif <federico.luzzi@proton.me>
2025-03-05 13:51:47 +0000

    feat: rename variable

--- app/commons.ts
+++ app/commons.ts
@@ -1,3 +1,3 @@
-const PRODUCTION = process.env.NODE_ENV === "production";
+const PRODUCTION = process.env.NEXT_PUBLIC_PRODUCTION === "true";
 
 export const REMOTE_MDX_BRANCH = PRODUCTION ? "main" : "develop";

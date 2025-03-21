36954dcd61740cf1354ce26572e0d254efd2f6b6
Paolo Guerra <pg.guerrap@proton.me>
2025-03-21 17:53:01 +0100

    feat: add documentation for new hooks, minor fixes

--- app/react-library/_meta.ts
+++ app/react-library/_meta.ts
@@ -3,8 +3,11 @@ const meta = {
         type: "separator",
         title: "Hooks",
     },
+    "use-campaign": "useCampaign",
     "use-campaigns": "useCampaigns",
     "use-claims": "useClaims",
+    "use-claims-transaction": "useClaimTransactions",
+    "use-leaderboard": "useLeaderboard",
 };
 
 export default meta;

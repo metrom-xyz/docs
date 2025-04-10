1b1d64326335e7e387911dc305aaa926e6ccaf48
Paolo Guerra <pg.guerrap@proton.me>
2025-04-10 15:02:12 +0200

    chore: upgrade dependencies

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+import { FlatCompat } from "@eslint/eslintrc";
+
+const compat = new FlatCompat({
+    baseDirectory: import.meta.dirname,
+});
+
+const eslintConfig = [
+    ...compat.config({
+        extends: ["next/core-web-vitals", "plugin:prettier/recommended"],
+        rules: { "prettier/prettier": ["error", { endOfLine: "auto" }] },
+    }),
+];
+
+export default eslintConfig;

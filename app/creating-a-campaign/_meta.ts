12ab23ed74e9c361c6cd14d98847241e723d21b8
afk1997 <kaivandoshi1997@gmail.com>
2025-05-09 16:34:24 +0530

    feat: reward ratio page

--- app/creating-a-campaign/_meta.ts
+++ app/creating-a-campaign/_meta.ts
@@ -1,4 +1,5 @@
 const meta = {
+    "reward-ratio": "Add Reward Ratio",
     "kpi-campaign": "Add a KPI",
     "rbi-campaign": "Add Range Based Incentive",
     "address-restrictions": "Add address restrictions",

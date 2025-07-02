e67a90b1252f8b1b50462b8878307e8069f01e96
afk1997 <kaivandoshi1997@gmail.com>
2025-07-02 22:17:44 +0530

    feat: removed for liquidity providers page from campaign creation

--- app/creating-a-campaign/_meta.ts
+++ app/creating-a-campaign/_meta.ts
@@ -3,7 +3,6 @@ const meta = {
     "kpi-campaign": "Add a KPI",
     "rbi-campaign": "Add Range Based Incentive",
     "address-restrictions": "Add address restrictions",
-    "liquidity-providers": "For liquidity providers",
 };
 
 export default meta;
